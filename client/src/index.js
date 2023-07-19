2b5ad8c7c2559716301b87545c193882e8b7adf6
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-07-19 21:02:16 +0800

    feat: single Provider page holds 9 carrier

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Form from "./pages/Form";
-// import AU from "./pages/AU";
-// import Docomo from "./pages/Docomo";
-// import GTN from "./pages/GTN";
-// import Linemo from "./pages/Linemo";
-// import Mobal from "./pages/Mobal";
-// import Rakuten from "./pages/Rakuten";
-// import Softbank from "./pages/Softbank";
-// import UQ from "./pages/UQ";
-// import Ymobile from "./pages/Ymobile";
+
 import Provider from "./pages/Provider"
 
 const router = createBrowserRouter([
