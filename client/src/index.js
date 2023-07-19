5f1eca9fffc2dd1cd60cd9e02138192f3b9a3dae
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-07-19 15:39:25 +0800

    change path to using one Provider page

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,16 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Form from "./pages/Form";
-import AU from "./pages/AU";
-import Docomo from "./pages/Docomo";
-import GTN from "./pages/GTN";
-import Linemo from "./pages/Linemo";
-import Mobal from "./pages/Mobal";
-import Rakuten from "./pages/Rakuten";
-import Softbank from "./pages/Softbank";
-import UQ from "./pages/UQ";
-import Ymobile from "./pages/Ymobile";
+// import AU from "./pages/AU";
+// import Docomo from "./pages/Docomo";
+// import GTN from "./pages/GTN";
+// import Linemo from "./pages/Linemo";
+// import Mobal from "./pages/Mobal";
+// import Rakuten from "./pages/Rakuten";
+// import Softbank from "./pages/Softbank";
+// import UQ from "./pages/UQ";
+// import Ymobile from "./pages/Ymobile";
+import Provider from "./pages/Provider"
 
 const router = createBrowserRouter([
   {
@@ -25,39 +26,48 @@ const router = createBrowserRouter([
   },
   {
     path: "/AU",
-    element: <AU />
+    // element: <AU />
+    element: <Provider provider_id={3}/>
   },
   {
     path: "/Docomo",
-    element: <Docomo />
+    // element: <Docomo />
+    element: <Provider provider_id={2} />
   },
   {
     path: "/GTN Mobile",
-    element: <GTN />
+    // element: <GTN />
+    element: <Provider provider_id={9}/>
   },
   {
     path: "/Linemo",
-    element: <Linemo />
+    // element: <Linemo />
+    element: <Provider provider_id={6} />
   },
   {
     path: "/Mobal",
-    element: <Mobal />
+    // element: <Mobal />
+    element: <Provider provider_id={1}/>
   },
   {
     path: "/Rakuten Mobile",
-    element: <Rakuten />
+    // element: <Rakuten />
+    element: <Provider provider_id={5} />
   },
   {
     path: "/Softbank",
-    element: <Softbank/>
+    // element: <Softbank/>
+    element: <Provider provider_id={4} />
   },
   {
     path: "/UQ Mobile",
-    element: <UQ />
+    // element: <UQ />
+    element: <Provider provider_id={8} />
   },
   {
     path: "/Y!mobile",
-    element: <Ymobile />
+    // element: <Ymobile />
+    element: <Provider provider_id={7} />
   },
 ]);
 
