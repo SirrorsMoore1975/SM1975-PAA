a91584e8340dea07e014fb10df435612a53cd1a5
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-08-09 23:40:07 +0800

    provider can navigate left right - need fix

--- client/src/components/ProviderNavbar.jsx
+++ client/src/components/ProviderNavbar.jsx
@@ -6,66 +6,51 @@ import Button from './Button';
 import "../styles/ProviderNavbar.css";
 import providerJSON from "../data/provider.json";
 
-const options = [
-    {
-        "provider_id": 0,
-        "value": "errorpage",
-        "text": "undefined"
-    },
-    ...providerJSON
-];
+
 
 const ProviderNavbar = ({provider_id}) => {
-    // useEffect(() => {
-    //     fetchedCurrentProvider();
-    // },[]);
-    
-    const [prefill, setPrefill ] = useState(parseInt(provider_id));
+
+    const [options, setOptions] = useState([...providerJSON])
+    const fetchProvider = () => {
+        try {
+            if(options===[]){
+                setOptions([{    
+                    "provider_id": 0,
+                    "value": "errorpage",
+                    "text": "undefined"
+                }]);
+            }
+        } catch(error) {
+            console.error("Cannot fetch provider: ", error);
+        } finally {
+            console.log("fetch provider data is: ",options);
+        }
+        
+    }
+    useEffect(() => {
+        fetchProvider();
+        console.log("👹",options);
+    }, []);
+
+    const [prefill, setPrefill ] = useState(provider_id ?? 1);
     useEffect(()=>{
-        console.log(provider_id);
-        console.log(prefill);
-    },[prefill, provider_id]);
-    // const [providers, setProviders] = useState([]);
-    // useEffect(()=>{
-    //     const headers = {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //     }
-    //     const fetchData = async () => {
-    //         try{
-    //            const response = await axios.get("/api/providers", {headers: headers})
-    //            setProviders(...providers,response.data);
-    //            console.log("ProviderNavbar.jsx:", providers);
-    //         } catch(err) {
-    //             console.log(err);
-    //         }
-            
-    //     }
-    //     fetchData();
-    //     console.log(options);
-    // },[]);
+        
+        try{
+            if(!prefill){
+                console.error("provider_id has error: ",provider_id)
+            } else {
+                setPrefill(parseInt(provider_id));
+            }
+            console.log("🈲provider_id:",provider_id);
+            console.log("😇prefill:",prefill);
+
+        } catch (error){
+            console.error(error);
+        }
+    },[]);
     
-    // useEffect(()=>{
-    //     // provider_id = parseInt(prefill, 10);
-    //     let prefillNum = parseInt(prefill)
-    //     if(isNaN(prefillNum)){
-    //         setPrefill(0);
-    //     } else {
-    //         setPrefill(prefillNum);
-    //     }
-    //     console.log("🥰",prefillNum);
-    // }, [prefillNum])
-
-
     const navigate = useNavigate();
-    // const [provider, setProvider] = useState([{},[]]);
-
-    // const fetchedCurrentProvider = async () => {
-    //     const result = await axios.get(`/api/provider/${provider_id}`);
-    //     setProvider(result.data)
-    // }
-    
-    // const [currentView , setCurrentView] = useState('');
+  
     const [goLeft, setGoLeft] = useState(prefill)
     const [goRight, setGoRight] = useState(prefill);
 
@@ -74,21 +59,27 @@ const ProviderNavbar = ({provider_id}) => {
         setGoLeft(goLeft - 1)
         if(goLeft===0){
             setGoLeft(8);
+            setPrefill(goLeft);
+            navigate(`/${options[goLeft].value}`)
+        } else {
+            setPrefill(goLeft);
+            navigate(`/${options[goLeft].value}`);
         }
-        console.log("😂", goLeft);
-        setPrefill(goLeft);
-        navigate(`/${options[goLeft].value}`);
+        console.log("goLeft- 😂", goLeft);
     }
   
     const handleGoRight = (e) => {
         e.preventDefault();
         setGoRight(goRight + 1);
         if(goRight===9){
-            setGoRight(1);
+            setGoRight(0);
+            setPrefill(goRight);
+            navigate(`/${options[goRight].value}`)
+        } else {
+            setPrefill(goRight);
+            navigate(`/${options[goRight].value}`)
         }
-        console.log("🤪",goRight);
-        setPrefill(goRight);
-        navigate(`/${options[goRight].value}`);
+        console.log("goRight- 🤪",goRight);
     }
 
     const handleDropdownMenu = (e) => {
@@ -97,7 +88,7 @@ const ProviderNavbar = ({provider_id}) => {
     return (
         <>
             <div className="provider-navbar">
-                <div className="provider-pannel"><Button className="button" text="<" onClick={handleGoLeft} />{"Select: "}<Button className="button" text={(options[prefill].text) ? options[prefill].text : options[0].text} onClick={handleDropdownMenu}/><Button className="button" text=">" onClick={handleGoRight} />
+                <div className="provider-pannel"><Button className="button" text="<" onClick={handleGoLeft} />{"Select: "}<Button className="button" text={options[2].text} onClick={handleDropdownMenu}/><Button className="button" text=">" onClick={handleGoRight} />
                 </div>
             </div>
         </>
