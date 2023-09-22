68b020929ffdb199206787ce91897dccfce0cbeb
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-09-22 12:15:49 +0800

    moved ReviewForm.jsx #030 code to #031

--- client/src/pages/ReviewForm.jsx
+++ client/src/pages/ReviewForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {useParams} from "react-router-dom";
 
 import axios from 'axios';
 import Navbar from '../components/Navbar';
@@ -11,9 +12,30 @@ import Radio from '../components/Radio';
 
 import ThankYou from '../pages/ThankYou';
 
+import providerJSON from "../data/provider.json";
 import "../styles/Form.css";
 
-const reviewData = {
+const createReviewData = (provider_id,reviewer_name,email,overall,ease_of_use,coverage,price,customer_service,customer_review) => {
+  return {
+    provider_id,
+    reviewer_name,
+    email,
+    overall,
+    ease_of_use,
+    coverage,
+    price,
+    customer_service,
+    customer_review
+  }
+}
+
+const header = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+}
+
+const reviewData = 
+{
   provider_id: "", 
   reviewer_name: "", 
   email: "", 
@@ -26,32 +48,33 @@ const reviewData = {
 };
 
 const ReviewForm = () => {
+  const { id } = useParams();
   
-  const [, getOverAllScore] = useState(0);
+  const [overAllScore, getOverAllScore] = useState(0);
   const setOverAllScore = (score) =>{
       getOverAllScore(score);
       reviewData.overall = Number(score);
   }
 
-  const [, getEOUScore] = useState(0);
+  const [EOUScore, getEOUScore] = useState(0);
   const setEOUScore = (score) =>{
     getEOUScore(score);
     reviewData.ease_of_use = Number(score);
   }
 
-  const [, getCoverageScore] = useState(0);
+  const [coverageScore, getCoverageScore] = useState(0);
   const setCoverageScore = (score) =>{
     getCoverageScore(score);
     reviewData.coverage = Number(score);
   }
 
-  const [, getPriceScore] = useState(0);
+  const [priceScore, getPriceScore] = useState(0);
   const setPriceScore = (score) =>{
     getPriceScore(score);
     reviewData.price = Number(score);
   }
 
-  const [, getCustomerServiceScore] = useState(0);
+  const [customerServiceScore, getCustomerServiceScore] = useState(0);
   const setCustomerServiceScore = (score) =>{
     getCustomerServiceScore(score);
     reviewData.customer_service= Number(score);
@@ -68,21 +91,29 @@ const ReviewForm = () => {
 
   // EMAIL STATE
   const [email, setEmail] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(true);
 
   const handleEmailInput = (event) => {
     const value = event.target.value;
     setEmail(value);
+   
     reviewData.email = value;
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const isValid = emailRegex.test(value);
+    if(isValid){
+      console.log("email is valid:",value);
+    }
   }
 
   // DROP DOWN MENU STATE
-  const [, getCompName] = useState('');
-  const setCompName = (compName) => {
+  reviewData.provider_id= Number(id);
+  // const [, getCompName] = useState('');
+  // const setCompName = (compName) => {
     
-    getCompName(compName);
-    reviewData.provider_id = Number(compName);
+  //   getCompName(compName);
+  //   reviewData.provider_id= Number(id);
     
-  }
+  // }
     
   // COMMENT STATE
   const [comment, setComment] = useState('');
@@ -96,20 +127,88 @@ const ReviewForm = () => {
   
   // SERVER RESPONSE STATE
   const [serverResponse, setServerResponse] = useState('Pending');
+  const [attemptedSubmit, setAttemptedSubmit] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [finalConfirmWindows, setFinalConfirmWindows] = useState(null)
   
+  const handleFinalConfirmWindows = (event) => {
+    event.preventDefault();
+    setIsActive(true);
+    
+    const result = (
+      <>
+        <div className={`confirm-window-container`}> 
+        <nav className={`confirm-window ${isActive ? 'active' : 'inactive'}`}>
+          <ul>
+            <li>{`Provider: ${providerJSON[id - 1].text}`}</li>
+            <li>{`Reviewer name: ${nickname}`}</li>
+            <li>{`Email: ${email}`}</li>
+            <li>{`Overall score: ${overAllScore}`}</li>
+            <li>{`Ease of use score: ${EOUScore}`}</li>
+            <li>{`Coverage score: ${coverageScore}`}</li>
+            <li>{`Price score: ${priceScore}`}</li>
+            <li>{`Customer service score: ${customerServiceScore}`}</li>
+          </ul>
+          <div className="confirm-panel">
+            <span>
+              <button onClick={(e)=> {e.preventDefault(); setIsActive(false); }}>{`Cancel`}</button>
+              </span>
+            <span>
+              <button onClick={ handleSubmission }>{`Confirm`}</button>
+              </span>
+          </div>
+          </nav>
+        </div>
+      </>
+    )
+    if(isActive){
+
+      setFinalConfirmWindows(result);
+    } else {
+      setFinalConfirmWindows(null);
+    }
+  }
+
   // HANDLER FUNCTION
   const handleSubmission = async (event) => {
     event.preventDefault();
-    setIsSubmitted(!isSubmitted);
-    // const response = 
-    const response = await axios.post('/api/review', reviewData)
-      .catch((error) => console.log(error));  
-    setServerResponse(response.data);
+    // Check name, email and textarea before allowing the submission of the review to be written into the database
+    // They cannot be empty
+    // email cannot be repeated (may required a new api to handle this request and get response, true should warn this email been use for this review already - might required feature such as login user with password to edit comment in the future; false should check the next item)
+    // textarea: 1.) must not be empty; 2.) must have at least 10 characters long
     
-  };
+    try {
+      
+      const response = await axios.post('/api/review', reviewData, header)
+      const responseData = await response.data
+        const usedWithProvider = responseData?.usedWithProvider;
+        const message = responseData?.message;
+        if(!usedWithProvider){
+          setIsSubmitted(!isSubmitted);
+          setAttemptedSubmit(false);
+          setAlertMessage(null);
+          setServerResponse(message);
+        } 
+      
+    } catch (error){
+      if(typeof error.data === "string" && error.data.startsWith("\n<!doctype html>\n")){
+        const responseErrorData = error.data;
+        const errorWindow = window.open('', '_blank');
+        errorWindow.document.write(responseErrorData);
+        errorWindow.document.close()
+      } 
+      // else if (!Array.isArray(error.data)){
 
-  
+      // }
+      const response = error.response.data;
+      const responseMessage = response.message
+      setAttemptedSubmit(true);
+      setAlertMessage(responseMessage);
+      console.error(error);
+    }
+  };
 
   return (
     <>  
@@ -173,10 +272,13 @@ const ReviewForm = () => {
                   value={ email }
                   onChange={ handleEmailInput }
                 />
+                {attemptedSubmit ? <><div className="alert-submited-email">{alertMessage}</div></> : null}
+                
+                <div>
 
-                {<DropdownMenu
-                  className="menu"  
-                  setProviderId={setCompName}/>}
+                {`You are currently reviewing:`} <br />
+                <span className="provider-highlight">{`${providerJSON[id - 1].text}`}</span>
+                </div>
                   
               </div>
             </div>
@@ -189,17 +291,22 @@ const ReviewForm = () => {
                   value={ comment }
                   onChange={ handleCommentInput }
                 />
+                {finalConfirmWindows}
             <Button
             className="button submit"
               text="Submit"
-              onClick = { handleSubmission }
+              onClick = { handleFinalConfirmWindows }
             />
+             <div className={`final-confirm-windows ${isActive ? 'active' : 'inactive'}`}>
+      {isActive ? <>{finalConfirmWindows}</>: null}
+    </div>
             <Footer className="footer" text="© 2023 Phone Carrier Review App"/>
         </div>
         
         )
       }
     </div>
+   
     </>
   );
 }
